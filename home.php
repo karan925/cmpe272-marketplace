922daacb57c79ceaacfe0b7cc078feb1cc2d419e
asadabbasi200 <35577736+asadabbasi200@users.noreply.github.com>
2020-11-18 22:11:31 -0800

    Update home.php

--- home.php
+++ home.php
@@ -1,14 +1,30 @@
 <!DOCTYPE html>
 <html>
 <header>
-    Test
+<meta charset="utf-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+   <meta http-equiv="x-ua-compatible" content="ie=edge">
+
+   <!-- Bootstrap CSS -->
+   <!-- build:css css/main.css -->
+   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+      integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+   <link rel="stylesheet" href="style.css">
+
+<div class="topnav">
+  <a class="active" href="./home.php">Home</a>
+  <a href="./companies_link.php">Marketplace Members</a>
+  <a href="./reviews.php">Add a Review</a>
+  <a href="./marketplace_popular.php">Most Popular Products</a>
+  <a href="./visit_history.php">My History</a>
+  <a href="./authenticate.php">Log In/Sign Up</a>
+</div>
 </header>
 <body>
 
-<h1>Marketplace Heading </h1>
-<p>My first paragraph.</p>
-<p>Commit from Jason</p>
-<p>Commit from Asad</p>
+<h1>AJKM Marketplace</h1>
+<p>Pictures and stuff can go here.</p>
 
 </body>
 </html>
