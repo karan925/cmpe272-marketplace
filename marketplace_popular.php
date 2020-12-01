a9fb45ec07bbab9b385e4f4e955208ae5c221b02
asadabbasi200 <35577736+asadabbasi200@users.noreply.github.com>
2020-12-01 00:33:24 -0800

    Update marketplace_popular.php

--- marketplace_popular.php
+++ marketplace_popular.php
@@ -22,6 +22,7 @@
   <a href="./authenticate.php">Log In/Sign Up</a>
 </div>
 </header>
+<meta http-equiv="refresh" content="10" >
 <body>
 
 <h3>Presentation of the top 5 products in the whole marketplace (most visits/best ratings etc.)</h3>
@@ -48,15 +49,17 @@
           $unserialized = $x['products_list'];
       }
       $prod_list = unserialize($unserialized);
-      echo '<table class="table"><tr><th>Products</th></tr>';
+      arsort($prod_list);
+      $keys = array_keys($prod_list);
+      echo '<table class="table"><tr></tr>';
       $count = 0;
-      foreach ($prod_list as $page){
+      foreach ($keys as $page){
          if ($count == 5) {
          break;
          }
         $filt = substr($page, 1);
         $name = explode(".", $filt)[0];
-        $link = "http://localhost:6969".$page;
+        $link = ".".$page;
         echo '<tr><td><a href="'.$link.'"><strong>'.$name.'</strong></a></td></tr>';
         $count = $count + 1;
      }
@@ -65,6 +68,134 @@
    echo '</div>';
    echo '</div>';
    echo '</div>';
+# -----------------------------------------NEW SITE-------------------
+   echo '<div class="Container">';
+   echo '<div class="rpw">';
+   echo '<div class="col-6 offset-2">';
+   echo "<h4>Most Viewed Products at Potato Inc.</h4>";
+   $query = "SELECT * FROM heroku_8c6c26a69cb9c50.most_visited_per WHERE company = 'Potato'";
+   $con = mysqli_connect("us-cdbr-east-02.cleardb.com", "b74d7cacca644f", "96adc723");
+   mysqli_select_db($con, "heroku_8c6c26a69cb9c50");
+   if ( !( $result = mysqli_query($con, $query))) {
+      print("Could not execute query! <br />");
+      die();
+   }
+   if (mysqli_num_rows($result) == 0) {
+         echo "No Product History for Potato Inc. at the moment.";
+   }
+   else {
+      $query = "SELECT products_list FROM heroku_8c6c26a69cb9c50.most_visited_per WHERE company = 'Potato'";
+      $result = mysqli_query($con, $query);
+      $unserialized = array();
+      foreach ($result as $x) {
+          $unserialized = $x['products_list'];
+      }
+      $prod_list = unserialize($unserialized);
+      arsort($prod_list);
+      $keys = array_keys($prod_list);
+      echo '<table class="table"><tr></tr>';
+      $count = 0;
+      foreach ($keys as $page){
+         if ($count == 5) {
+         break;
+         }
+        $filt = substr($page, 1);
+        $name = explode(".", $filt)[0];
+        $link = ".".$page;
+        echo '<tr><td><a href="'.$link.'"><strong>'.$name.'</strong></a></td></tr>';
+        $count = $count + 1;
+     }
+     echo '</table>';
+  }   
+   echo '</div>';
+   echo '</div>';
+   echo '</div>';
+
+#---------------------------NEW SITE------------------------------
+echo '<div class="Container">';
+echo '<div class="rpw">';
+echo '<div class="col-6 offset-2">';
+echo "<h4>Most Viewed Products at Trucking Inc.</h4>";
+$query = "SELECT * FROM heroku_8c6c26a69cb9c50.most_visited_per WHERE company = 'Trucking'";
+$con = mysqli_connect("us-cdbr-east-02.cleardb.com", "b74d7cacca644f", "96adc723");
+mysqli_select_db($con, "heroku_8c6c26a69cb9c50");
+if ( !( $result = mysqli_query($con, $query))) {
+   print("Could not execute query! <br />");
+   die();
+}
+if (mysqli_num_rows($result) == 0) {
+      echo "No Product History for Trucking Inc. at the moment.";
+}
+else {
+   $query = "SELECT products_list FROM heroku_8c6c26a69cb9c50.most_visited_per WHERE company = 'Trucking'";
+   $result = mysqli_query($con, $query);
+   $unserialized = array();
+   foreach ($result as $x) {
+       $unserialized = $x['products_list'];
+   }
+   $prod_list = unserialize($unserialized);
+   arsort($prod_list);
+   $keys = array_keys($prod_list);
+   echo '<table class="table"><tr></tr>';
+   $count = 0;
+   foreach ($keys as $page){
+      if ($count == 5) {
+      break;
+      }
+     $filt = substr($page, 1);
+     $name = explode(".", $filt)[0];
+     $link = ".".$page;
+     echo '<tr><td><a href="'.$link.'"><strong>'.$name.'</strong></a></td></tr>';
+     $count = $count + 1;
+  }
+  echo '</table>';
+}   
+echo '</div>';
+echo '</div>';
+echo '</div>';
+
+#----------------------NEW SITE-------------------------------
+echo '<div class="Container">';
+echo '<div class="rpw">';
+echo '<div class="col-6 offset-2">';
+echo "<h4>Most Viewed Products at Comics Inc.</h4>";
+$query = "SELECT * FROM heroku_8c6c26a69cb9c50.most_visited_per WHERE company = 'Comic'";
+$con = mysqli_connect("us-cdbr-east-02.cleardb.com", "b74d7cacca644f", "96adc723");
+mysqli_select_db($con, "heroku_8c6c26a69cb9c50");
+if ( !( $result = mysqli_query($con, $query))) {
+   print("Could not execute query! <br />");
+   die();
+}
+if (mysqli_num_rows($result) == 0) {
+      echo "No Product History for Comics Inc. at the moment.";
+}
+else {
+   $query = "SELECT products_list FROM heroku_8c6c26a69cb9c50.most_visited_per WHERE company = 'Comic'";
+   $result = mysqli_query($con, $query);
+   $unserialized = array();
+   foreach ($result as $x) {
+       $unserialized = $x['products_list'];
+   }
+   $prod_list = unserialize($unserialized);
+   arsort($prod_list);
+   $keys = array_keys($prod_list);
+   echo '<table class="table"><tr></tr>';
+   $count = 0;
+   foreach ($keys as $page){
+      if ($count == 5) {
+      break;
+      }
+     $filt = substr($page, 1);
+     $name = explode(".", $filt)[0];
+     $link = ".".$page;
+     echo '<tr><td><a href="'.$link.'"><strong>'.$name.'</strong></a></td></tr>';
+     $count = $count + 1;
+  }
+  echo '</table>';
+}   
+echo '</div>';
+echo '</div>';
+echo '</div>';
 ?>
 
 </body>
