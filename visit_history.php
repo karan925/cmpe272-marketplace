12d60545c003b16bcaf2da552305d8d78c7bb036
asadabbasi200 <35577736+asadabbasi200@users.noreply.github.com>
2020-12-08 19:26:05 -0800

    Update visit_history.php

--- visit_history.php
+++ visit_history.php
@@ -28,6 +28,40 @@
 <h3>Your Previously Visited Products</h3>
 <p>The list of all the user's product visit history will go here.</p>
 <?php 
+   $name_mapper = array();
+   $name_mapper['product1'] = '2019 Porsche 911';
+   $name_mapper['product2'] = '2018 Mercedes-Benz AMG C63 S';
+   $name_mapper['product3'] = '2020 Tesla Roadster';
+   $name_mapper['product4'] = '2020 Audi R8 5.2 V10 Performance';
+   $name_mapper['product5'] = '2017 Ferrari 488 Spider Base';
+   $name_mapper['product6'] = '2019 Mclaren F1';
+   $name_mapper['product7'] = '2018 Lamborghini Aventador S';
+   $name_mapper['product8'] = '2016 BMW M3';
+   $name_mapper['product9'] = '2019 BMW M6';
+   $name_mapper['product10'] = '2019 Mercedes G-Wagon AMG';
+   $name_mapper['products/FondantPotato'] = 'Fondant Potato';
+   $name_mapper['products/RattePotato'] = 'Ratte Potato';
+   $name_mapper['products/RedPotato'] = 'Red Potato';
+   $name_mapper['products/Fries'] = 'French Fries';
+   $name_mapper['products/GreenPotato'] = 'Green Potato';
+   $name_mapper['products/RoastedPotato'] = 'Roasted Potato';
+   $name_mapper['products/KerrPotato'] = 'Kerr Potato';
+   $name_mapper['products/Vitelotte'] = 'Vitellote';
+   $name_mapper['products/PotatoJuice'] = 'Potato Juice';
+   $name_mapper['products/YukonPotato'] = 'Yukon Potato';
+   $name_mapper['hw/products/hw5_product1'] = 'A Scanner Darkly';
+   $name_mapper['hw/products/hw5_product2'] = 'Blade Runner: The Final Cut';
+   $name_mapper['hw/products/hw5_product3'] = 'Ghost in the Shell (2020 4k BD)';
+   $name_mapper['hw/products/hw5_product4'] = 'Minority Report';
+   $name_mapper['hw/products/hw5_product5'] = 'Psycho-Pass (BD/DVD Combo)';
+   $name_mapper['hw/products/hw5_product6'] = 'Do Androids Dream of Electric Sheep?';
+   $name_mapper['hw/products/hw5_product7'] = 'Dune';
+   $name_mapper['hw/products/hw5_product8'] = 'Neuromancer';
+   $name_mapper['hw/products/hw5_product9'] = 'The Foundation Trilogy';
+   $name_mapper['hw/products/hw5_product10'] = 'Ubik';
+   $trucking = array('product1', 'product2', 'product3', 'product4', 'product5', 'product6', 'product7', 'product8', 'product9', 'product10');
+   $potatos = array('products/FondantPotato', 'products/RattePotato', 'products/RedPotato', 'products/Fries', 'products/GreenPotato', 'products/RoastedPotato', 'products/KerrPotato', 'products/Vitelotte', 'products/PotatoJuice', 'products/YukonPotato');
+   $sfdb = array('hw/products/hw5_product1', 'hw/products/hw5_product2', 'hw/products/hw5_product3', 'hw/products/hw5_product4', 'hw/products/hw5_product5', 'hw/products/hw5_product6', 'hw/products/hw5_product7', 'hw/products/hw5_product8', 'hw/products/hw5_product9', 'hw/products/hw5_product10');
 // echo $_SESSION['username'];
    $con = mysqli_connect("us-cdbr-east-02.cleardb.com", "b74d7cacca644f", "96adc723");
 
@@ -65,12 +99,28 @@
          }
          $history = array_reverse(unserialize($unserialized));
          $ozone = array('/Skyscraper.php', '/Museums.php', '/TrainStation.php', '/Houses.php', '/Airports.php', '/Apartments.php', '/BusinessComplex.php', '/ShoppingComplex.php', '/Stadiums.php', '/Landmarks.php');
-         
+
          foreach ($history as $page) {
-            $name = substr($page, 1,-4);
+            $name1 = substr($page, 1,-4);
             $searchpos1 = array_search($page, $ozone);
+            $searchpos2 = array_search($name1, $trucking);
+            $searchpos3 = array_search($name1, $potatos);
+            $searchpos4 = array_search($name1, $sfdb);
             if ($searchpos1 !== FALSE) {
                $link = 'http://myozone.org/'.$page;
+               $name = $name1;
+            }
+            if ($searchpos2 !== FALSE) {
+               $link = 'https://karantrucking.herokuapp.com/'.$page;
+               $name = $name_mapper[$name1];
+            }
+            if ($searchpos3 !== FALSE) {
+               $link = "https://liuj.us/".$page;
+               $name = $name_mapper[$name1];
+            }
+            if ($searchpos4 !== FALSE) {
+               $link = "http://www.cmpe272mustafay.com/".$page;
+               $name = $name_mapper[$name1];
             }
             echo '<ol><a href="'.$link.'"><strong>'.$name.'</strong></a></ol>';
          }
