d4135296be6872bf4c6d3656a51879d218b6976f
jasonliu96 <fmbxjason@gmail.com>
2020-12-09 20:19:31 -0800

    Update reviewForm.php

--- reviewForm.php
+++ reviewForm.php
@@ -6,22 +6,27 @@
 <body>
 	<form id="reviewForm">
 		<div class="input-group mb-3">
-		<label for="ratings">Please Choose a Rating</label>
-		<input type="radio" name="ratings" id="one" value="one">
-		<label for="one">One</label>
-		<input type="radio" name="ratings" id="two" value="two">
-		<label for="two">Two</label>
-		<input type="radio" name="ratings" id="three" value="three">
-		<label for="three">Three</label>
-		<input type="radio" name="ratings" id="four" value="four">
-		<label for="four">Four</label>
-		<input type="radio" name="ratings" id="five" value="five">
-		<label for="five">Five</label>
+			<label for="ratings"><Strong>Please Choose a Rating</Strong></label>
+			<br>
+			<input type="radio" name="ratings" id="one" value="one">
+			<label for="one">One</label>
+			<input type="radio" name="ratings" id="two" value="two">
+			<label for="two">Two</label>
+			<input type="radio" name="ratings" id="three" value="three">
+			<label for="three">Three</label>
+			<input type="radio" name="ratings" id="four" value="four">
+			<label for="four">Four</label>
+			<input type="radio" name="ratings" id="five" value="five">
+			<label for="five">Five</label>
+		</div>
+		<div>
+			<br>
+			<label for="review"><Strong>Please Submit a Review</Strong></label>
+			<br>
+			<textarea form="reviewForm" rows="4" cols="50" name="review" id="review">Enter Review Here.	
+			</textarea>	
+		</div>
 		<br>
-		<label for="review"><Strong>Please Submit a Review</Strong></label>
-		<textarea form="reviewForm" rows="4" cols="50" name="review" id="review">
-			Enter Review Here. 	
-		</textarea>	
 		<input type="submit"> 
 	</form>
 </body>
