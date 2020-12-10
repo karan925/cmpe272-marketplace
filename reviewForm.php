6af2261dfc7ce1a9172ff72b1b44d3bbbe5e0206
jasonliu96 <fmbxjason@gmail.com>
2020-12-09 20:32:47 -0800

    Update reviewForm.php
    style sheet + top nav

--- reviewForm.php
+++ reviewForm.php
@@ -1,9 +1,27 @@
 <!DOCTYPE html>
 <html>
 <header>
-	<h1> Submit a Review </h1>
+	<meta charset="utf-8">
+   	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+   	<meta http-equiv="x-ua-compatible" content="ie=edge">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+      integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+	<link rel="stylesheet" href="style.css">
+
+		<div class="topnav">
+		<a href="./home.php">Home</a>
+		<a href="./companies_link.php">Marketplace Members</a>
+		<a class = "active" href="./reviews.php">Add a Review</a>
+		<a href="./marketplace_popular.php">Most Popular Products</a>
+		<a href="./visit_history.php">My History</a>
+		<a href="./authenticate.php">Log In/Sign Up</a>
+		<a href="./logout.php">Log Out</a>
+		</div>
+
 </header>
 <body>
+
+	<h1> Submit a Review </h1>
 	<form id="reviewForm">
 		<div class="input-group mb-3">
 			<label for="ratings"><Strong>Please Choose a Rating</Strong></label>
