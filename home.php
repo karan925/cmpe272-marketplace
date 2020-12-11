651058bd455b4f2166caf762e732eaf0e35379e9
Karandeep Singh <karan@Karandeeps-MacBook-Pro.local>
2020-12-11 00:21:31 -0800

    Added another product to photo slideshow

--- home.php
+++ home.php
@@ -114,21 +114,24 @@
     </script>' ; 
     ?> 
   </div>
+ 
+  <br>
+  <br>
 
   <div class="container">    
   <div class="row">
     <div class="col-sm-4">
       <div class="panel panel-primary">
         <div class="panel-heading">BLACK FRIDAY DEAL</div>
-        <div class="panel-body"><img src="https://placehold.it/150x80?text=IMAGE" class="img-responsive" style="width:100%" alt="Image"></div>
-        <div class="panel-footer">Buy 50 mobiles and get a gift card</div>
+        <div class="panel-body"><img src="pics/v.jpg" class="img-responsive" style="width:100%" alt="Image"></div>
+        <div class="panel-footer">Buy 50 or more and get a 15% discount!</div>
       </div>
     </div>
     <div class="col-sm-4"> 
       <div class="panel panel-danger">
         <div class="panel-heading">BLACK FRIDAY DEAL</div>
-        <div class="panel-body"><img src="https://placehold.it/150x80?text=IMAGE" class="img-responsive" style="width:100%" alt="Image"></div>
-        <div class="panel-footer">Buy 50 mobiles and get a gift card</div>
+        <div class="panel-body"><img src="pics/redPotato.jpg" class="img-responsive" style="width:100%" alt="Image"></div>
+        <div class="panel-footer">Buy 50 or more and get a 15% discount!</div>
       </div>
     </div>
     <div class="col-sm-4"> 
