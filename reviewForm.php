be2847b35c9e79d00e42bc20d2e8d677377579fa
MustafaYesilyurt <myesilyurt700@gmail.com>
2020-12-11 15:58:52 -0800

    process review

--- reviewForm.php
+++ reviewForm.php
@@ -92,11 +92,10 @@
 	</form>
 	<script>
 		window.onload = function() {
-				let prod_name = document.getElementById('product_n').textContent;
-				let comp_name = document.getElementById('company_n').textContent;
-				document.getElementById('product_name').value = prod_name;
-				document.getElementById('company_name').value = comp_name;
-			}
+			let prod_name = document.getElementById('product_n').textContent;
+			let comp_name = document.getElementById('company_n').textContent;
+			document.getElementById('product_name').value = prod_name;
+			document.getElementById('company_name').value = comp_name;
 		};
 	</script>
 </body>
