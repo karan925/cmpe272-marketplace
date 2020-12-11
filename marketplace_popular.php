4ba485d0781ea4f5b6150cde8f5410a6eeaede60
asadabbasi200 <35577736+asadabbasi200@users.noreply.github.com>
2020-12-10 23:34:00 -0800

    Update marketplace_popular.php

--- marketplace_popular.php
+++ marketplace_popular.php
@@ -59,6 +59,10 @@
    $name_mapper['hw/products/hw5_product8'] = 'Neuromancer';
    $name_mapper['hw/products/hw5_product9'] = 'The Foundation Trilogy';
    $name_mapper['hw/products/hw5_product10'] = 'Ubik';
+   $trucking = array('product1', 'product2', 'product3', 'product4', 'product5', 'product6', 'product7', 'product8', 'product9', 'product10');
+   $potatos = array('products/FondantPotato', 'products/RattePotato', 'products/RedPotato', 'products/Fries', 'products/GreenPotato', 'products/RoastedPotato', 'products/KerrPotato', 'products/Vitelotte', 'products/PotatoJuice', 'products/YukonPotato');
+   $sfdb = array('hw/products/hw5_product1', 'hw/products/hw5_product2', 'hw/products/hw5_product3', 'hw/products/hw5_product4', 'hw/products/hw5_product5', 'hw/products/hw5_product6', 'hw/products/hw5_product7', 'hw/products/hw5_product8', 'hw/products/hw5_product9', 'hw/products/hw5_product10');
+   $ozone = array('/Skyscraper.php', '/Museums.php', '/TrainStation.php', '/Houses.php', '/Airports.php', '/Apartments.php', '/BusinessComplex.php', '/ShoppingComplex.php', '/Stadiums.php', '/Landmarks.php');
    echo '<div class="Container">';
    echo '<div class="rpw">';
    echo '<div class="col-6 offset-2">';
@@ -82,6 +86,7 @@
       }
       $prod_list = unserialize($unserialized);
       arsort($prod_list);
+      $ozone_list = $prod_list;
       $keys = array_keys($prod_list);
       echo '<table class="table"><tr></tr>';
       $count = 0;
@@ -124,6 +129,7 @@
       }
       $prod_list = unserialize($unserialized);
       arsort($prod_list);
+      $pot_list = $prod_list;
       $keys = array_keys($prod_list);
       echo '<table class="table"><tr></tr>';
       $count = 0;
@@ -168,6 +174,7 @@ else {
    }
    $prod_list = unserialize($unserialized);
    arsort($prod_list);
+   $kem_list = $prod_list;
    $keys = array_keys($prod_list);
    echo '<table class="table"><tr></tr>';
    $count = 0;
@@ -212,6 +219,7 @@ else {
    }
    $prod_list = unserialize($unserialized);
    arsort($prod_list);
+   $sfdb_list = $prod_list;
    $keys = array_keys($prod_list);
    echo '<table class="table"><tr></tr>';
    $count = 0;
@@ -228,6 +236,35 @@ else {
   }
   echo '</table>';
 }   
+echo '</div>';
+echo '</div>';
+echo '</div>';
+
+#------- TOTAL MARKETPLACE ------------------------------
+echo '<div class="Container">';
+echo '<div class="rpw">';
+echo '<div class="col-6 offset-2">';
+echo "<h4>Most Viewed Products in the Marketplace</h4>";
+// $query = "SELECT * FROM heroku_8c6c26a69cb9c50.most_visited_per WHERE company = 'SFDB'";
+
+$total_list = $ozone_list + $pot_list + $kem_list + $sfdb_list;
+arsort($total_list);
+$keys = array_keys($total_list);
+echo '<table class="table"><tr></tr>';
+$count = 0;
+foreach ($keys as $page){
+   if ($count == 5) {
+   break;
+   }
+   $filt = substr($page, 1);
+   $name1 = explode(".", $filt)[0];
+   $name = $name_mapper[$name1];
+   $link = ".".$page;
+   echo '<tr><td><a href="'.$link.'"><strong>'.$name.'</strong></a></td></tr>';
+   $count = $count + 1;
+}
+echo '</table>';
+  
 echo '</div>';
 echo '</div>';
 echo '</div>';
