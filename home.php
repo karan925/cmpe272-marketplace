4c6edca141264e67e2e435f807bbff672fb2d537
Karandeep Singh <karan@Karandeeps-MacBook-Pro.local>
2020-12-11 00:53:16 -0800

    Added another product to photo slideshow

--- home.php
+++ home.php
@@ -94,6 +94,7 @@
    echo '</div>';
  }
  ?>
+
   <div class="container">
     <div class="jumbotron">
       <h1>AJKM Marketplace</h1>
@@ -101,37 +102,41 @@
   </div>
   <h2>Welcome!</h2>
   <h5>Check Out the Recently Sold Items below!</h5>
-  <div class="w3-content w3-display-container">
-    <img class="mySlides" src="pics/car.jpg" width="400" height="400">
-    <img class="mySlides" src="pics/pot.jpg" width="400" height="400">
-    <img class="mySlides" src="pics/build.jpg" width="400" height="400">
-    <img class="mySlides" src="pics/star.jpg" width="400" height="400">
+  <div class="container"> 
+  <div id="myCarousel" class="carousel slide" data-ride="carousel">
+    <!-- Indicators -->
+    <ol class="carousel-indicators">
+      <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+      <li data-target="#myCarousel" data-slide-to="1"></li>
+      <li data-target="#myCarousel" data-slide-to="2"></li>
+    </ol>
 
-    <button class="w3-button w3-black w3-display-left" onclick="plusDivs(-1)">&#10094;</button>
-    <button class="w3-button w3-black w3-display-right" onclick="plusDivs(1)">&#10095;</button>
-    <?php  
-    echo '<script type="text/JavaScript">  
+    <!-- Wrapper for slides -->
+    <div class="carousel-inner">
+      <div class="item active">
+        <img src="pics/car.jpg" alt="Los Angeles" style="width:50%;">
+      </div>
 
-    var slideIndex = 1;
-    showDivs(slideIndex);
+      <div class="item">
+        <img src="pics/m1.jpg"alt="Chicago" style="width:50%;">
+      </div>
+    
+      <div class="item">
+        <img src="pics/h1.jpg" alt="New york" style="width:50%;">
+      </div>
+    </div>
 
-    function plusDivs(n) {
-      showDivs(slideIndex += n);
-    }
-
-    function showDivs(n) {
-      var i;
-      var x = document.getElementsByClassName("mySlides");
-      if (n > x.length) {slideIndex = 1}
-      if (n < 1) {slideIndex = x.length}
-      for (i = 0; i < x.length; i++) {
-        x[i].style.display = "none";  
-      }
-      x[slideIndex-1].style.display = "block";  
-    }
-    </script>' ; 
-    ?> 
+    <!-- Left and right controls -->
+    <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+      <span class="glyphicon glyphicon-chevron-left"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a class="right carousel-control" href="#myCarousel" data-slide="next">
+      <span class="glyphicon glyphicon-chevron-right"></span>
+      <span class="sr-only">Next</span>
+    </a>
   </div>
+</div>
  
   <br>
   <br>
