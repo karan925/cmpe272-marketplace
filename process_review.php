c74cdffee41037918edd1941251afbabe3322699
asadabbasi200 <35577736+asadabbasi200@users.noreply.github.com>
2020-12-12 16:14:00 -0800

    Update process_review.php

--- process_review.php
+++ process_review.php
@@ -37,7 +37,7 @@
 			$Username = $x['username'];
 			$loggedin = $x['loggedin'];
 		}
-		if ($loggedin == 1) {
+	
 		$select_review_sql = "SELECT * FROM heroku_8c6c26a69cb9c50.review WHERE name = "."'".$Username."' ". "AND item = "."'".$product_name."'";
 		if ( !( $result = mysqli_query($con, $select_review_sql))) {
 			print("Could not execute query! <br />");
@@ -103,8 +103,5 @@
 	echo "<h2>Thank you! Your review and rating have been successfully submitted!</h2>";
 	echo '<a href="./companies_link.php"><button>Check out more of our products!</button></a>';
 }
-else {
-	echo "Log in to add a review";
-    echo '<a href="./authenticate.php"><button>Log In Here!</button></a>';
-}
+
 ?>
